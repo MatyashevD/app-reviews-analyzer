0165161df8b5156a82d2619773be2cdd70b2855d
MatyashevD <dmatyashev@yandex.ru>
2025-02-18 20:40:37 +0300

    app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def search_apps(query: str):
                 "entity": "software,iPadSoftware",
                 "lang": "ru_ru"
             },
-            headers={"User-Agent": "Mozilla/5.0"}
+            headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
         )
         ios_data = itunes_response.json()
         
