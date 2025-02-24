3b285884f740e61ea8cc3b495fb140b0f21a9f15
Dmitry Matyashev <dmatyashev@yandex.ru>
2025-02-24 23:21:42 +0300

    app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def main():
                 }
                 .app-card {
                     display: inline-block;
-                    width: 260px;
+                    width: 300px;
                     border: 1px solid #e0e0e0;
                     border-radius: 12px;
                     padding: 12px;
