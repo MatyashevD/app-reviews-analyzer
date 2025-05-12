41d04107227e2e6c02b758b11d27f099ed25a590
Dmitry Matyashev <dmatyashev@yandex.ru>
2025-05-12 23:35:20 +0300

    app.py

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ def main():
                         country=country.lower(),
                         app_name=app_name,
                         app_id=str(app_id)
-                    
+                    )
                     app.review(
                         how_many=max_reviews,
                         sleep=random.uniform(2, 4))
